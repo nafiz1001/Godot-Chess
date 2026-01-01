a8c061beb9fb2a51034fe0db1b4c424d5f0b5b9b
nafiz1001 <nafiz.islam1000@gmail.com>
2026-01-01 17:19:20 -0500

    init project
    - add chess board sprite
    - add chess piece sprite
    - add tool-mode script for chess piece

---
+++ ChessPiece.gd
@@ -0,0 +1,38 @@
+@tool
+extends Node2D
+
+static var texture = preload("res://Chess_Pieces_Sprite.svg")
+var atlas_texture = AtlasTexture.new()
+var sprite = Sprite2D.new()
+
+enum ChessPiece {KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN}
+enum ChessColour {WHITE, BLACK}
+
+var _piece: ChessPiece = ChessPiece.KING
+var _colour: ChessColour = ChessColour.WHITE
+
+@export var piece: ChessPiece:
+	get():
+		return _piece
+	set(value):
+		_piece = value
+		update_atlas()
+
+@export var colour: ChessColour:
+	get():
+		return _colour
+	set(value):
+		_colour = value
+		update_atlas()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	atlas_texture.atlas = texture
+	piece = _piece
+	colour = _colour
+	
+	sprite.texture = atlas_texture
+	add_child(sprite)
+
+func update_atlas():
+	atlas_texture.region = Rect2(135 * piece, 135 * colour, 135, 135)
