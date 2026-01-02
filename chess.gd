4fee363ed2244bfcd702afb18aea66cc92e31783
nafiz1001 <nafiz.islam1000@gmail.com>
2026-01-02 12:06:52 -0500

    wip

--- chess.gd
+++ chess.gd
@@ -1,8 +1,10 @@
+@tool
 extends Node2D
 
-var chess_board: ChessBoardNode
-var chess_piece: ChessPieceNode
+var chess_board: ChessBoardNode = null
+var chess_grid: Array[Array] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	chess_board = load("res://chess_board.tscn").instantiate()
+	add_child(chess_board)
--- chess_board.gd
+++ chess_board.gd
@@ -1,5 +1,20 @@
+@tool
 class_name ChessBoardNode
 extends Node2D
 
 const LENGTH = 810
 const CELL_LENGTH = LENGTH / 8
+
+const COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+const ROWS =    ['1', '2', '3', '4', '5', '6', '7', '8']
+
+
+func _ready() -> void:
+	for row in range(8):
+		for col in range(8):
+			var node = Node2D.new()
+			node.position = Vector2(CELL_LENGTH * col + CELL_LENGTH / 2, CELL_LENGTH * row + CELL_LENGTH / 2)
+			node.name = COLUMNS[col] + ROWS[row]
+			if Engine.is_editor_hint():
+				node.add_child(Marker2D.new())
+			add_child(node)
