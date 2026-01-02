12643bef2a0177a84423641bbfe1d61c19ee922b
nafiz1001 <nafiz.islam1000@gmail.com>
2026-01-02 16:14:39 -0500

    wip lazy cell creation

--- chess_board.gd
+++ chess_board.gd
@@ -10,11 +10,29 @@ const ROWS =    ['1', '2', '3', '4', '5', '6', '7', '8']
 
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		add_markers()
+
+func add_markers():
 	for row in range(8):
 		for col in range(8):
 			var node = Node2D.new()
 			node.position = Vector2(CELL_LENGTH * col + CELL_LENGTH / 2, CELL_LENGTH * row + CELL_LENGTH / 2)
 			node.name = COLUMNS[col] + ROWS[row]
-			if Engine.is_editor_hint():
-				node.add_child(Marker2D.new())
+			node.add_child(Marker2D.new())
 			add_child(node)
+
+func indices_to_coordinates(column: int, row: int):
+	return COLUMNS[column] + ROWS[row]
+
+func put_in_cell(column: int, row: int, node: Node):
+	var coordinates = indices_to_coordinates(column, row)
+	var cell = find_child(coordinates)
+	if cell:
+		cell.add_child(node)
+	else:
+		cell = Node2D.new()
+		cell.position = Vector2(CELL_LENGTH * column + CELL_LENGTH / 2, CELL_LENGTH * row + CELL_LENGTH / 2)
+		cell.name = coordinates
+		add_child(cell)
+		put_in_cell(column, row, node)
