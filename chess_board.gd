44ff066c427d14b6200df4bdea009e2960ddf650
nafiz1001 <nafiz.islam1000@gmail.com>
2026-01-03 15:08:41 -0500

    use area2d

--- chess_board.gd
+++ chess_board.gd
@@ -2,23 +2,20 @@
 class_name ChessBoardNode
 extends Node2D
 
-const LENGTH = 810
+const BORDER_PADDING = 5
+const LENGTH = (810 - BORDER_PADDING*2)
 const CELL_LENGTH = LENGTH / 8
 
 const COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 const ROWS =    ['8', '7', '6', '5', '4', '3', '2', '1']
 
-
 func _ready() -> void:
 	for row in range(8):
 		for col in range(8):
-			var cell = Node2D.new()
-			cell.position = Vector2(CELL_LENGTH * col + CELL_LENGTH / 2, CELL_LENGTH * row + CELL_LENGTH / 2)
+			var cell = create_area2d()
+			cell.position = Vector2(CELL_LENGTH * (col + 0.5) + BORDER_PADDING, CELL_LENGTH * (row + 0.5) + BORDER_PADDING)
 			cell.name = indices_to_coords(col, row)
 			$Cells.add_child(cell)
-
-	if Engine.is_editor_hint():
-		add_markers()
 
 func add_markers():
 	for cell in $Cells.get_children():
@@ -29,3 +26,11 @@ func indices_to_coords(col: int, row: int):
 
 func get_cell(col: int, row: int):
 	return $Cells.find_child(indices_to_coords(col, row), false, false)
+
+static func create_area2d():
+	var area = Area2D.new()
+	var shape = CollisionShape2D.new()
+	shape.shape = RectangleShape2D.new()
+	shape.shape.size = Vector2(CELL_LENGTH, CELL_LENGTH)
+	area.add_child(shape)
+	return area
